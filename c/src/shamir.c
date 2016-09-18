57d4a37f87cccb36bffe835b84d1da3b9de4fc8b
Mohamed Bamakhrama <mohamed@alumni.tum.de>
2016-09-18 02:28:45 +0200

    Ensure unique generation of random arrays
    This patch adds a function to ensure that the generated coefficients
    and x shares are unique.

--- c/src/shamir.c
+++ c/src/shamir.c
@@ -7,139 +7,166 @@
 
 #include "shamir.h"
 
-#define SUCCESS (EXIT_SUCCESS)
+
+static void gen_unique_random_list(
+	const size_t list_length,
+	const size_t element_size,
+	mpz_t * list,
+	gmp_randstate_t rng_state)
+{
+	size_t i = 0, j = 0;
+	int is_unique = 0;
+
+	assert(list != NULL);
+	assert(list_length > 0);
+	assert(element_size > 0);
+	assert(rng_state != NULL);
+
+	for (i = 0; i < list_length; i++) {
+		mpz_init(list[i]);
+		is_unique = 0;
+		while (!is_unique) {
+spin:			mpz_urandomb(list[i], rng_state, element_size);
+			mpz_add_ui(list[i], list[i], 1);
+			for (j = 0; j < i; j++) {
+				if (mpz_cmp(list[i], list[j]) == 0) {
+					goto spin;
+				}
+			}
+			is_unique = 1;
+		}
+	}
+}
 
 int split_secret(const mpz_t secret,
-                 const unsigned int num_shares,
-                 const unsigned int threshold,
-                 const mpz_t prime,
-                 mpz_t * shares_xs,
-                 mpz_t * shares_ys)
+		const unsigned int num_shares,
+		const unsigned int threshold,
+		const mpz_t prime,
+		mpz_t * shares_xs,
+		mpz_t * shares_ys)
 {
-    unsigned int i = 0, j = 0;
-    size_t prime_size = 0;
-    int retval = SUCCESS;
-    mpz_t * coefficients = NULL;
-    mpz_t y, tmp, degree;
-    gmp_randstate_t rng_state;
+	unsigned int i = 0, j = 0;
+	size_t prime_size = 0;
+	int retval = EXIT_SUCCESS;
+	mpz_t * coefficients = NULL;
+	mpz_t y, tmp, degree;
+	gmp_randstate_t rng_state;
 
-    /* Check the inputs */
-    if (mpz_cmp(secret, prime) >= 0 ||
-        shares_xs == NULL || shares_ys == NULL ||
-        threshold > num_shares || threshold < 1 || num_shares < 1) {
-        return EINVAL;
-    }
-    
-    coefficients = (mpz_t *) malloc((threshold - 1) * sizeof(mpz_t));
-    if (coefficients == NULL) {
-        return ENOMEM;
-    }
+	/* Check the inputs */
+	if (mpz_cmp(secret, prime) >= 0 || shares_xs == NULL ||
+		shares_ys == NULL || threshold > num_shares ||
+		threshold < 1 || num_shares < 1) {
+		return EINVAL;
+	}
 
-    srand(time(NULL));
-    gmp_randinit_default(rng_state);
-    gmp_randseed_ui(rng_state, rand());
-    prime_size = mpz_sizeinbase(prime, 2);
- 
-    /* Initialize coefficients and shares_xs */   
-    for (i = 0; i < (threshold - 1); i++) {
-        mpz_init(coefficients[i]);
-        mpz_urandomb(coefficients[i], rng_state, prime_size - 1);
-        mpz_add_ui(coefficients[i], coefficients[i], 1);
-    }
-    
-    for (i = 0; i < num_shares; i++) {
-        mpz_init(shares_xs[i]);
-        mpz_urandomb(shares_xs[i], rng_state, prime_size - 1);
-        mpz_add_ui(shares_xs[i], shares_xs[i], 1);
-    }
+	/*
+	This is the only place where we need malloc. If this code needs
+	to be ported into an "malloc-less" platform, then the following
+	needs to be replaced with a statically allocated coefficients
+	array with pre-defined maximum threshold.
+	*/
+	coefficients = (mpz_t *) malloc((threshold - 1) * sizeof(mpz_t));
+	if (coefficients == NULL) {
+		return ENOMEM;
+	}
 
-    mpz_init(tmp);
-    for (i = 0; i < num_shares; i++) {
-        mpz_init_set(y, secret);
-        mpz_init_set_ui(degree, 1);
-        for (j = 0; j < (threshold - 1); j++) {
-            mpz_powm_sec(tmp, shares_xs[i], degree, prime);
-            mpz_addmul(y, coefficients[j], tmp);
-            mpz_add_ui(degree, degree, 1);
-        }
-        mpz_clear(degree);
-        mpz_init(shares_ys[i]);
-        mpz_mod(y, y, prime);
-        mpz_set(shares_ys[i], y);
-        mpz_clear(y);
-        if (mpz_cmp(shares_xs[i], secret) == 0 ||
-            mpz_cmp(shares_ys[i], secret) == 0) {
-            retval = EINVAL;
-            break;
-        }
+	srand(time(NULL));
+	gmp_randinit_default(rng_state);
+	gmp_randseed_ui(rng_state, rand());
+	prime_size = mpz_sizeinbase(prime, 2);
 
-    }
-    mpz_clear(tmp);
+	/* Initialize coefficients and shares_xs */
+	gen_unique_random_list(threshold-1, prime_size-1,
+		coefficients, rng_state);
+	gen_unique_random_list(num_shares, prime_size-1,
+		shares_xs, rng_state);
 
-    if (retval != SUCCESS) {
-        for (i = 0; i < num_shares; i++) {
-            mpz_init_set_ui(shares_xs[i], 0);
-            mpz_init_set_ui(shares_ys[i], 0);
-        }
-    }
+	mpz_init(tmp);
+	for (i = 0; i < num_shares; i++) {
+		mpz_init_set(y, secret);
+		mpz_init_set_ui(degree, 1);
+		for (j = 0; j < (threshold - 1); j++) {
+			mpz_powm_sec(tmp, shares_xs[i], degree, prime);
+			mpz_addmul(y, coefficients[j], tmp);
+			mpz_add_ui(degree, degree, 1);
+		}
+		mpz_clear(degree);
+		mpz_init(shares_ys[i]);
+		mpz_mod(y, y, prime);
+		mpz_set(shares_ys[i], y);
+		mpz_clear(y);
+		if (mpz_cmp(shares_xs[i], secret) == 0 ||
+			mpz_cmp(shares_ys[i], secret) == 0) {
+			retval = EINVAL;
+			break;
+		}
+	}
+	mpz_clear(tmp);
 
-    gmp_randclear(rng_state);
-    /* Clear data */
-    for (i = 0; i < (threshold - 1); i++) {
-        mpz_clear(coefficients[i]);
-    }
-    free(coefficients);
-    coefficients = NULL;
+	if (retval != EXIT_SUCCESS) {
+		for (i = 0; i < num_shares; i++) {
+			mpz_init_set_ui(shares_xs[i], 0);
+			mpz_init_set_ui(shares_ys[i], 0);
+		}
+	}
 
-    return retval;
+	gmp_randclear(rng_state);
+	/* Clear data */
+	for (i = 0; i < (threshold - 1); i++) {
+		mpz_clear(coefficients[i]);
+	}
+	free(coefficients);
+	coefficients = NULL;
+
+	return retval;
 }
 
 
 int reconstruct_secret(const unsigned int num_shares,
-                       const mpz_t * shares_xs,
-                       const mpz_t * shares_ys,
-                       const mpz_t prime,
-                       mpz_t secret)
+		const mpz_t * shares_xs,
+		const mpz_t * shares_ys,
+		const mpz_t prime,
+		mpz_t secret)
 {
-    unsigned int j = 0, m = 0;
-    int retval = -1;
-    mpz_t product, d, r;
-    mpz_t reconstructed;
+	unsigned int j = 0, m = 0;
+	int retval = -1;
+	mpz_t product, d, r;
+	mpz_t reconstructed;
 
-    if (shares_xs == NULL || shares_ys == NULL) {
-        return EINVAL;
-    }
+	if (shares_xs == NULL || shares_ys == NULL) {
+		return EINVAL;
+	}
 
-    for (j = 0; j < num_shares; j++) {
-        if (mpz_cmp(shares_xs[j], prime) >= 0 ||
-            mpz_cmp(shares_ys[j], prime) >= 0) {
-            return EINVAL;
-        }
-    }
-    
-    mpz_init_set_ui(reconstructed, 0);
-    
-    for (j = 0; j < num_shares; j++) {
-        mpz_init_set_ui(product, 1);
-        for (m = 0; m < num_shares; m++) {
-            mpz_init(d);
-            mpz_init(r);
-            if (m != j) {
-                mpz_sub(d, shares_xs[m], shares_xs[j]);
-                retval = mpz_invert(d, d, prime);
-                assert(retval != 0);
-                mpz_mul(r, shares_xs[m], d);
-                mpz_mul(product, product, r);
-            }
-            mpz_clear(d);
-            mpz_clear(r);
-        }
-        mpz_addmul(reconstructed, shares_ys[j], product);
-        mpz_mod(reconstructed, reconstructed, prime);
-        mpz_clear(product);
-    }
-    mpz_init_set(secret, reconstructed);
-    mpz_clear(reconstructed);
+	for (j = 0; j < num_shares; j++) {
+		if (mpz_cmp(shares_xs[j], prime) >= 0 ||
+			mpz_cmp(shares_ys[j], prime) >= 0) {
+			return EINVAL;
+		}
+	}
 
-    return SUCCESS;
+	mpz_init_set_ui(reconstructed, 0);
+
+	for (j = 0; j < num_shares; j++) {
+		mpz_init_set_ui(product, 1);
+		for (m = 0; m < num_shares; m++) {
+			mpz_init(d);
+			mpz_init(r);
+			if (m != j) {
+				mpz_sub(d, shares_xs[m], shares_xs[j]);
+				retval = mpz_invert(d, d, prime);
+				assert(retval != 0);
+				mpz_mul(r, shares_xs[m], d);
+				mpz_mul(product, product, r);
+			}
+			mpz_clear(d);
+			mpz_clear(r);
+		}
+		mpz_addmul(reconstructed, shares_ys[j], product);
+		mpz_mod(reconstructed, reconstructed, prime);
+		mpz_clear(product);
+	}
+	mpz_init_set(secret, reconstructed);
+	mpz_clear(reconstructed);
+
+	return EXIT_SUCCESS;
 }
--- c/src/shamir.h
+++ c/src/shamir.h
@@ -3,6 +3,25 @@
 
 #include <gmp.h>
 
+/**
+ * @brief Splits the given \a secret into \a num_shares shares
+ *
+ * This function implements the \a secret splitting mechanism defined in:
+ * Adi Shamir, "How to share a secret", Communications of the ACM 22(11)
+ * pages 612-613 doi:10.1145/359168.359176
+ *
+ * The secret is split into \a num_shares that are stored as pairs of
+ * (x,y) in the given \a shares_xs and \a shares_ys. All the numbers must
+ * be less than the given \a prime
+ *
+ * @param secret The secret to be split
+ * @param num_shares The number of generated shares
+ * @param threshold The minimum number of shares needed to reconstruct secret
+ * @param prime A prime number used to construct a Galois field
+ * @param shares_xs The list of the x-component of the shares
+ * @param shares_ys The list of the y-component of the shares
+ * @return EXIT_SUCCESS if successful. Otherwise, EINVAL or ENOMEM
+ */
 int split_secret(const mpz_t secret,
                  const unsigned int num_shares,
                  const unsigned int threshold,
@@ -10,6 +29,18 @@ int split_secret(const mpz_t secret,
                  mpz_t * shares_xs,
                  mpz_t * shares_ys);
 
+/**
+ * @brief Reconstruct a secret from the given shares
+ *
+ * This function reconstruct the \a secret from the given \a shares_xs
+ * and \a shares_ys according to Shamir's algorithm.
+ *
+ * @param num_shares The number of shares
+ * @param shares_xs The list of the x-component of the shares
+ * @param shares_ys The list of the y-component of the shares
+ * @param prime A prime number used to construct a Galois field
+ * @param secret The reconstructed secret
+ */
 int reconstruct_secret(const unsigned int num_shares,
                        const mpz_t * shares_xs,
                        const mpz_t * shares_ys,
